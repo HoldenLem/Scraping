e36df4432f611df5fae855f694a4760b16f546f0
Hanna Lembak <lembakhanna@gmail.com>
2025-06-26 10:57:14 +0000

    maim result

--- src/main/java/obs/techstars/Scraping/ScrapingApplication.java
+++ src/main/java/obs/techstars/Scraping/ScrapingApplication.java
@@ -1,6 +1,7 @@
 package obs.techstars.Scraping;
 
-import obs.techstars.Scraping.scraping.MainPageScraper;
+import obs.techstars.Scraping.job.model.JobDto;
+import obs.techstars.Scraping.job.JobService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,15 +9,17 @@ import org.springframework.context.ApplicationContext;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @SpringBootApplication
 public class ScrapingApplication {
 
-	public static void main(String[] args) throws MalformedURLException, URISyntaxException {
-		ApplicationContext context = SpringApplication.run(ScrapingApplication.class, args);
-		MainPageScraper scrapingService = context.getBean(MainPageScraper.class);
-		scrapingService.getJobListingsBy("Design");
+    public static void main(String[] args) throws MalformedURLException, URISyntaxException {
+        ApplicationContext context = SpringApplication.run(ScrapingApplication.class, args);
 
-	}
+        JobService jobService = context.getBean(JobService.class);
+        List<JobDto> jobs = jobService.saveJobDetailsByFunction("Software Engineering");
+        System.out.println("Saved jobs: " + jobs.size());
+    }
 
 }
